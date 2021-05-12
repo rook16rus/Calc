6b0911ea4b713a4f1fb195724681ee3758e246e1
rook16rus <kamalov00albert@gmail.com>
2021-05-12 14:20:06 +0300

    Completed all tasks from the technical task

--- js/app.js
+++ js/app.js
@@ -12,3 +12,69 @@ for (var keys = document.querySelectorAll("#calculator span"), operators = ["+",
     } else "." == btnVal ? decimalAdded || (input.innerHTML += btnVal, decimalAdded = !0) : input.innerHTML += btnVal;
     e.preventDefault()
 };
+
+/*const arr = ['cat', 'dog', 'clown', 'snake', 'begemoth', 'tiger'];
+for (let i = 0; i < arr.length; i++)
+{
+    if ((i % 2) == 0)
+    {
+        console.log(arr[i]);
+    }
+}*/
+
+/*========*/
+
+/*
+const cats =
+    {
+        'abyssinian': '😸',
+        'american-bobtail' : '🐱',
+        'siam': '🙀',
+        'bombay' : '😼'
+    }
+
+for (key in cats)
+{
+    if (key.length <= 4)
+    {
+        console.log(key);
+    }
+}*/
+
+/*========*/
+
+const someObject = {
+
+};
+
+let addButton = document.getElementById('key_add');
+let result = document.querySelector('.result');
+let keyInput = document.getElementById('key');
+let valueInput = document.getElementById('value');
+
+addButton.onclick = function ()
+{
+    someObject[keyInput.value] = valueInput.value;
+    result.insertAdjacentHTML("afterbegin", '<p>' + keyInput.value + ': ' + valueInput.value +'</p>');
+};
+
+/*========*/
+
+/*
+const d = [0, 2, 5, -4, 6, 22, -9, -12, 13, 78];
+const a = [];
+const b = [];
+
+for (let i = 0; i < d.length; i++)
+{
+    if ((d[i] % 2) == 0 )
+    {
+        a.push(d[i]);
+    } else {
+        b.push(d[i]);
+    }
+}
+
+console.log(a);
+console.log(b);
+*/
