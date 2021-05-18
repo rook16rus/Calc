3024bdb796dcbdb2d0c6187ba2d4b69d1e181a7e
rook16rus <kamalov00albert@gmail.com>
2021-05-18 16:32:14 +0300

    Put the mac os and idea folders in gitignore

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 for (var keys = document.querySelectorAll("#calculator span"), operators = ["+", "-", "x", "÷"], decimalAdded = !1, i = 0; i < keys.length; i++) keys[i].onclick = function (e) {
-    var input = document.querySelector(".screen"), inputVal = input.innerHTML, btnVal = this.innerHTML, total;
+    var input = document.querySelector(".screen");
+    let inputVal = input.innerHTML;
+    let btnVal = this.innerHTML;
+    let total;
     if ("=" == btnVal) {
-        var equation = inputVal, lastChar = equation[equation.length - 1];
-        equation = equation.replace(/x/g, "*").replace(/÷/g, "/"), (operators.indexOf(lastChar) > -1 || "." == lastChar) && (equation = equation.replace(/.$/, "")), equation && (total = eval(equation), -1 != total.toString().indexOf(".") && (total = total.toFixed(2)), input.innerHTML = total), decimalAdded = !1
+        var equation = inputVal;
+        let lastChar = equation[equation.length - 1];
+        equation = equation.replace(/x/g, "*").replace(/÷/g, "/");
+        if (operators.includes(lastChar) || "." == lastChar) {
+            equation = equation.replace(/.$/, "")
+        }
+        equation && (total = eval(equation), -1 != total.toString().indexOf(".") && (total = total.toFixed(2)), input.innerHTML = total);
+        decimalAdded = false;
     } else if ('C' == btnVal) {
         input.innerHTML = '';
-    }
-    else if (operators.indexOf(btnVal) > -1) {
+    } else if (operators.indexOf(btnVal) > -1) {
         var lastChar = inputVal[inputVal.length - 1];
         ("" != inputVal && -1 == operators.indexOf(lastChar) || "" == inputVal && "-" == btnVal) && (input.innerHTML += btnVal), operators.indexOf(lastChar) > -1 && inputVal.length > 1 && (input.innerHTML = inputVal.replace(/.$/, btnVal)), decimalAdded = !1
     } else "." == btnVal ? decimalAdded || (input.innerHTML += btnVal, decimalAdded = !0) : input.innerHTML += btnVal;
@@ -43,19 +51,16 @@ for (key in cats)
 
 /*========*/
 
-const someObject = {
-
-};
+const someObject = {};
 
 let addButton = document.getElementById('key_add');
 let result = document.querySelector('.result');
 let keyInput = document.getElementById('key');
 let valueInput = document.getElementById('value');
 
-addButton.onclick = function ()
-{
+addButton.onclick = function () {
     someObject[keyInput.value] = valueInput.value;
-    result.insertAdjacentHTML("afterbegin", '<p>' + keyInput.value + ': ' + valueInput.value +'</p>');
+    result.insertAdjacentHTML("afterbegin", '<p>' + keyInput.value + ': ' + valueInput.value + '</p>');
 };
 
 /*========*/
